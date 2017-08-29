ab1fca5df46bf6aeaba387b02f685504911c82a7
wangyuanhsiang <ywang91@studentmail.nait.ca>
2017-08-28 20:29:33 -0600

    Shawn 2017/08/28

--- src/timer/JavaTimer.java
+++ src/timer/JavaTimer.java
@@ -5,6 +5,7 @@
  */
 package timer;
 
+import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -18,6 +19,7 @@ public class JavaTimer {
      * @param args the command line arguments
      */
     private int secondsPassed;
+    private int secondslossed;
     private Timer myTimer;
     private TimerTask task = new TimerTask() {
 
@@ -25,15 +27,41 @@ public class JavaTimer {
         public void run() {          
             secondsPassed++;
             System.out.println("Seconds Passed: " + secondsPassed);    
-            while (secondsPassed > 9 ) { 
-               stop();
-            }
+//            while (secondsPassed > 9 ) { 
+//               stop();
+//            }
+            
         }
     };
+    
+     private TimerTask taskTwo = new TimerTask() {
+
+        @Override
+        public void run() {          
+            secondslossed--;
+            System.out.println("Seconds Lossed: " + secondslossed);    
+            while (secondslossed <= 0 ) { 
+               stop();
+            }
+            
+        }
+    };
+    
 
     public void start() { 
         myTimer = new Timer();
         myTimer.schedule(task, 1000, 1000); //Seconds
+//        myTimer.schedule(task, 60000, 60000); // Minutes
+//        myTimer.schedule(task, 3600000, 3600000); // Hours
+    }
+    
+    public void startTimer() { 
+        myTimer = new Timer();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Seconds:");    
+        secondslossed = scanner.nextInt();
+        
+        myTimer.schedule(taskTwo, 1000, 1000); //Seconds
 //        myTimer.schedule(task, 60000, 60000); // Minutes
 //        myTimer.schedule(task, 3600000, 3600000); // Hours
     }
@@ -46,7 +74,8 @@ public class JavaTimer {
         // TODO code application logic here
         JavaTimer timer = new JavaTimer();    
 //        while (timer.secondsPassed < 11 ) {            
-           timer.start(); 
+//           timer.start(); 
+        timer.startTimer();
 //        }
 //          timer.stop();
         
