34d9a78429cd3276826a3d6a79bd31e5ae3634fd
wangyuanhsiang <ywang91@studentmail.nait.ca>
2017-08-28 20:03:20 -0600

    Revert "Shawn 2017/0828"
    This reverts commit e0121fc640e7c70cdf978daef6976a9947dc7024.

--- src/timer/JavaTimer.java
+++ src/timer/JavaTimer.java
@@ -5,10 +5,8 @@
  */
 package timer;
 
-import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
-
 
 /**
  *
@@ -20,7 +18,6 @@ public class JavaTimer {
      * @param args the command line arguments
      */
     private int secondsPassed;
-    private int secondslossed; 
     private Timer myTimer;
     private TimerTask task = new TimerTask() {
 
@@ -28,25 +25,12 @@ public class JavaTimer {
         public void run() {          
             secondsPassed++;
             System.out.println("Seconds Passed: " + secondsPassed);    
-//            while (secondsPassed > 9 ) { 
-//               stop();
-//            }
-        }
-    };
-
-    
-      private TimerTask task2 = new TimerTask() {
-
-        @Override
-        public void run() {          
-            secondslossed--;
-            System.out.println("Seconds Passed: " + secondslossed);    
-            while (secondslossed <= 0 ) { 
+            while (secondsPassed > 9 ) { 
                stop();
             }
         }
     };
-    
+
     public void start() { 
         myTimer = new Timer();
         myTimer.schedule(task, 1000, 1000); //Seconds
@@ -61,23 +45,11 @@ public class JavaTimer {
     public static void main(String[] args) {
         // TODO code application logic here
         JavaTimer timer = new JavaTimer();    
-        Scanner input = new Scanner(System.in);
-        System.out.println("enter seconds");
-        int value = input.nextInt();
-        timer.setsecondslossed(value);
-        timer.startTimer();
+//        while (timer.secondsPassed < 11 ) {            
+           timer.start(); 
+//        }
 //          timer.stop();
-        
-        
         
     }
 
-    public void setsecondslossed(int seconds){
-         this.secondslossed = seconds;
-    }
-    
-    public void startTimer(){
-       myTimer = new Timer();
-       myTimer.schedule(task2, 1000, 1000);
-    }
 }
